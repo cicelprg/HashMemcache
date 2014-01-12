5ffe8561915a4110228eee8cf974d7aa00653948
菜鸟贵 <scyaprg@163.com>
2014-01-12 21:29:01 +0800

    更改文件名confi 为config

---
+++ application/config/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 配置文件，这里直接用最简单的配置
+ * @package config
+ */
+
+/**
+ *mysql 数据库配置
+ */
+$GLOBALS['db_host'] = 'localhost';
+$GLOBALS['db_user'] = 'root';
+$GLOBALS['db_pwd' ] = '245030109';
+$GLOBALS['db_name'] = 'memcached';
+
+
+?>
