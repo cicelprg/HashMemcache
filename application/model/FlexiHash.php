6df5940a34c37207c97fa2b6847cc3c63117416f
菜鸟贵 <scyaprg@163.com>
2014-01-14 19:06:19 +0800

    更新核心类
    将引入路劲改变防止别人调用api时路径出错，采用绝对路径。增加了移除一个服务器后数据转存的功能

--- application/model/FlexiHash.php
+++ application/model/FlexiHash.php
@@ -6,20 +6,52 @@
  * @uses Severs
  * @uses Skey
  */
-require_once '../model/server.class.php';
-require_once '../model/skey.class.php';
-require_once '../base/memcache.class.php';
-require_once '../base/baseException.class.php';
+require_once realpath($_SERVER['DOCUMENT_ROOT']).'/HashMemcache/application/config/config.php';
+require_once $GLOBALS['memcached_path'].'/model/server.class.php';
+require_once $GLOBALS['memcached_path'].'/model/skey.class.php';
+require_once $GLOBALS['memcached_path'].'/base/memcache.class.php';
+require_once $GLOBALS['memcached_path'].'/base/baseException.class.php';
 class FlexiHash
 {
+	/**
+	 * 是否采用数据转存模式，会存储sid-key到数据库
+	 * 会加大对数据库的操作
+	 * @var bool
+	 */
+	private $safe = true;
+	/**
+	 * key的hash值
+	 * @var int
+	 */
 	private $hashKey;
+	/**
+	 * 服务器列表
+	 * @var array
+	 */
 	private $severs ;
+	/**
+	 * 服务器模型
+	 * @var obj
+	 */
 	private $severModel;
+	/**
+	 * sid-key 模型
+	 * @var obj
+	 */
 	private $skeyModel;
+	/**
+	 *标记是否排序
+	 */
 	private $isSort = false;
+	/**
+	 * 标记被移除的服务器在serverlist中的位置
+	 * @var int
+	 */
+	private $flag;
 	function __construct()
 	{
 		$this->severModel = new Severs();
+		$this->skeyModel  = new Skey();
 		$this->severs     = $this->severModel->getAllServers();
 		$this->sortRbyHash();
 		$this->isSort = true;
@@ -48,30 +80,61 @@ class FlexiHash
 	 */
 	function addSever($sever =array())
 	{
-		if(is_array($sever))
+		try
 		{
-			$this->isSort = false;
-			$key = $this->hash(implode('', $sever));
-			$sever['shash'] = $key;
-			//添加到数据库中
-			$this->severModel->addSever($sever);
-			
-			//这里改变存储结构
-			if(empty($this->severs[0]))
+			if(is_array($sever))
 			{
-				$this->severs[0] = $sever;
+				$this->isSort = false;
+				$key = $this->hash(implode('', $sever));
+				$sever['shash'] = $key;
+				//首先检查新天的服务器是否能够连接
+				if(!BaseMemcahce::connect($sever))
+				{
+					throw new BaseException('对不起不能连接到新服务器，添加失败');
+				}
+				else
+				{
+					//添加到数据库中
+					$sid = $this->severModel->addSever($sever);
+					if(!$sid)
+					{
+						throw new BaseException('对不起，系统内部出错!请稍后再试');
+					}
+					else if($sid==-1)
+					{
+						throw new BaseException("服务器".$sever['shost'].':'.$sever['sport']."已经存在了！");
+					}
+					else
+					{
+						
+						$sever['sid'] = (int)$sid;
+						//这里改变存储结构
+						if(empty($this->severs[0]))
+						{
+							$this->severs[0] = $sever;
+						}
+						else
+						{
+							$this->severs[] = $sever;
+						}
+						if(!$this->isSort)
+						{
+							$this->sortRbyHash();
+						}
+					}
+				}
 			}
 			else
 			{
-				$this->severs[] = $sever;
+				throw new BaseException('服务器不符合标准 ');
 			}
-			if(!$this->isSort)
-			{
-				$this->sortRbyHash();
-			}
-			
-			
 		}
+		catch (BaseException $be)
+		{
+			echo $be;
+			//exit;
+		}
+		
 	}
 	
 	/**
@@ -116,6 +179,10 @@ class FlexiHash
 		{
 			$this->sortRbyHash();
 		}
+		if(empty($this->severs)||empty($this->severs[0])) //这里服务器一个都没有
+		{
+			return false;
+		}
 		$this->hashKey = $this->hash($key);
 		foreach ($this->severs as $sever)
 		{
@@ -124,38 +191,239 @@ class FlexiHash
 				return $sever;
 			}
 		}
-		//这里没有找到对应的sever时返回最后一个,就是hashKey 小于了服务器最小的那个hash值
-		return $this->severs[count($this->severs)-1];
+		//这里没有找到对应的sever时返回第一个,就是hashKey小于了服务器最小的那个hash值,此时直接轮回
+		return $this->severs[0];
 	}
 	
+	/**
+	 * 数据保存
+	 * @param string $key
+	 * @param string $value
+	 * @throws BaseException
+	 */
 	function saveValue($key,$value)
+	{
+		$fp = fopen($GLOBALS['memcached_path'].'/log/mem.log', "a+");
+		if(empty($this->severs)||empty($this->severs[0]))
+		{
+			$str="Sorry，No server to save Data.---Time:".date("Y-m-d H:i:s",time())."\r\n";
+			fputs($fp, $str);
+			fclose($fp);
+			return;
+		}
+		$sever = $this->getSaveSever($key);
+		if(!is_array($sever))
+		{
+			$str="Sorry，We Cannot get The server to save Data.---Time:".date("Y-m-d H:i:s",time())."\r\n";
+			fputs($fp, $str);
+			fclose($fp);
+			return;
+		}
+		if(!@BaseMemcahce::connect($sever))
+		{
+			$str = "Sorry，Data Save Failed.We Cannot Connect to".$sever['shost'].
+			':'.$sever['sport']."Plesea check open the server---Time:".date("Y-m-d H:i:s",time())."\r\n";
+			fputs($fp, $str);
+			fclose($fp);
+			return;
+		}
+		if(!@BaseMemcahce::$mem[$sever['shash']]->set($this->hashKey,serialize($value)))
+		{
+			$str='Sorry，Data('.serialize($value).') Save Failed On'.$sever['shost'].
+			':'.$sever['sport']."---Time:".date("Y-m-d H:i:s",time())."\r\n";
+			fputs($fp, $str);
+			fclose($fp);
+			return;
+		}
+		else if($this->safe) //相对安全，保存对应的key值
+		{
+			$data = array(
+					'sid' =>$sever['sid'],
+					'skey'=>$this->hashKey
+					);
+			$this->skeyModel->addServerkey($data);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param string $key
+	 * @throws BaseException
+	 * @return boolean|mixed
+	 */
+	function getValue($key)
+	{
+		$fp = fopen($GLOBALS['memcached_path'].'/log/mem.log', "a+");
+		if(empty($this->severs)||empty($this->severs[0]))
+		{
+			$str="Sorry，No server to get Data.---Time:".date("Y-m-d H:i:s",time())."\r\n";
+			fputs($fp, $str);
+			fclose($fp);
+			return;
+		}
+		$server = $this->getSaveSever($key);
+		var_dump($server);
+		if(!BaseMemcahce::connect($server))
+		{
+			//这里命中率降低 //这里也只能写日志
+			//throw new BaseException("读取数据失败，不能连接到对应的服务器".$server['shost'].':'.$server['port']);
+			$str='Sorry，Read Data Failed.Cannot connect to'.$server['shost'].
+			':'.$server['sport']."---Time:".date("Y-m-d H:i:s",time())."\r\n";
+			fputs($fp, $str);
+			fclose($fp);
+			return false;
+		}
+		else
+		{
+			$res = BaseMemcahce::$mem[$server['shash']]->get($this->hashKey);
+			if(!$res)
+			{
+				return false;
+			}
+			else
+			{
+				//命中率变高
+				return unserialize($res);
+			}
+		}
+			
+	}
+	/**
+	 * 删除服务器时对数据的转存
+	 * @param array $server
+	 */
+	function DataTransfer($server=array(),$tranServer =array())
 	{
 		try 
 		{
-			$sever = $this->getSaveSever($key);
-			var_dump($this->hashKey);
-			var_dump($sever);
-			if(!is_array($sever))
+			$allkeys  = $this->skeyModel->getKeysBySid($server['sid']);
+			//var_dump($allkeys);
+			$num = count($allkeys);
+			$skid = array();
+			//连接到服务器
+			if(!BaseMemcahce::connect($server))
 			{
-				throw new BaseException("对不起，获取要保存的服务器失败");
+				throw new BaseException("warning:，不能连接要关闭的服务器".$server['shost'].':'.$server['sport']."不能进行数据转存!请关闭安全然后删除服务器");
 			}
-			if(!BaseMemcahce::connect($sever))
+			if(!BaseMemcahce::connect($tranServer))
 			{
-				throw new BaseException('对不起，服务器'.$sever['shost'].':'.$sever['sport'].'连接失败，请检查是否开启服务！');
+				throw new BaseException("Error:不能连接到目标服务器".$server['shost'].':'.$server['sport'].'请保证服务器打开 ');
 			}
-			//var_dump($this->hashKey);
-			if(!BaseMemcahce::$mem->set($this->hashKey,serialize($value)))
+			$k = 0;
+			//开始数据转存
+			for($i=0;$i<$num;$i++)
 			{
-				throw new BaseException('对不起，数据在'.$sever['shost'].':'.$sever['sport'].'服务器上保存失败 ');
+				$v = BaseMemcahce::$mem[$server['shash']]->get($allkeys[$i]);
+				if($v)
+				{
+					//var_dump($v);
+					if(BaseMemcahce::$mem[$tranServer['shash']]->set($allkeys[$i],$v))
+					{
+						$skid[] = $allkeys[$i];
+						$k++;
+					}
+				}
 			}
-			//echo BaseMemcahce::$mem->getVersion();
-			BaseMemcahce::close();
+			
+			//flushall
+			BaseMemcahce::$mem[$server['shash']]->flush();
+			//var_dump($skid);
+			//这里保存有效转移的数据
+			if($k>0)
+			{
+				$this->skeyModel->addTransKeys($skid, (int)$tranServer['sid']);
+			}
+			$data[$num] = $k;
+			return $data;
 		}
 		catch (BaseException $be)
 		{
 			echo $be;
 		}
+	}
+	
+	/**
+	 * 删除服务器 
+	 * @param array $server
+	 * @throws BaseException
+	 * @return number
+	 */
+	function removeServer($server =array())
+	{
+		try
+		{
+			if(count($this->severs)==1)
+			{
+				throw new BaseException("只有一个服务器，不能移除 ");
+			}
+			if(!$this->isSort)
+			{
+				$this->sortRbyHash();
+			}
+			if($this->safe)
+			{
+				$tranServer = $this->getTransToServer($server);
+				//var_dump($tranServer);
+				//exit;
+				if($tranServer == $server)
+				{
+					throw new BaseException("只有一个服务器，不能移除 ");
+				}
+				$data = $this->DataTransfer($server,$tranServer);
+			}
+			//这里移除成功 
+			$this->skeyModel->deleteKeysBySid($server['sid']);
+			$this->severModel->removeSever($server['sid']);
+			//var_dump($this->flag);
+			array_splice($this->severs, $this->flag,1);
+			$this->isSort =false;
+			if($this->safe)
+			{
+				return $data;
+			}
+		}catch (BaseException $be)
+		{
+			echo $be;
+		}
 		
+	}
+	
+	/**
+	 * 当一个服务器移除是，该服务器上的数据直接转移到hash环上的下一个服务器
+	 * @param array $server
+	 */
+	function getTransToServer($server =array())
+	{
+		$num = count($this->severs);
+		for($i=0;$i<$num;$i++)
+		{
+			if($this->severs[$i]['shash']==(int)$server['shash']) //这里找到要移除的服务器 
+			{
+				$this->flag = $i;
+				return $this->severs[($i+1)%$num];
+			}
+		}
+	}
+	
+	/**
+	 * 设置是否安全
+	 * @param bool $value
+	 */
+	function setSafe($value)
+	{
+		if(is_bool($value))
+		{
+			$this->safe = $value;
+		}
+	}
+	
+	/**
+	 * 获取安全状态
+	 * @return boolean
+	 */
+	function getSafe()
+	{
+		return $this->safe;
 	}
 }
 ?>
