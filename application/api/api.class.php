42e5e90c9973896fa7f927d8c3e6909e6f211552
菜鸟贵 <scyaprg@163.com>
2014-01-14 19:06:41 +0800

    对外调用api类
    还没有写任何内容

---
+++ application/api/api.class.php
@@ -0,0 +1,11 @@
+<?php
+require_once realpath($_SERVER['DOCUMENT_ROOT']).'/HashMemcache/application/model/FlexiHash.php';
+/**
+ * 提供api ，用户只需要引入这个类，调用方法就可以对memcached进行操作
+ * @author prg
+ */
+class HashMemcachedAPI
+{
+	
+}
+?>
