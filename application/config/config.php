8ed1eaca87e4e8783279a96fb4e27999d10e0877
菜鸟贵 <scyaprg@163.com>
2014-01-14 19:02:25 +0800

    更新配置文件
    增加路径配置，方便别人调用api

--- application/config/config.php
+++ application/config/config.php
@@ -12,5 +12,14 @@ $GLOBALS['db_user'] = 'root';
 $GLOBALS['db_pwd' ] = '245030109';
 $GLOBALS['db_name'] = 'memcached';
 
+/**
+ * mysql 表名称配置
+ */
+$GLOBALS['t_server'] ='t_severs';
+$GLOBALS['t_skey']   ='t_skey';
 
+/**
+ * 这里配置memcached基本路径 用户使用时要修改这里配置和model下面的FlexiHash.php 里面的第一个引入config文件的路径
+ */
+$GLOBALS['memcached_path'] =realpath($_SERVER['DOCUMENT_ROOT']).'/HashMemcache/application';
 ?>
